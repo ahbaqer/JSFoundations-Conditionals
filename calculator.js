f1ac444bdfb33a970ac75666a2338043a2567042
Abbas Baqer <ABaqer87@Abbass-MacBook-Pro.local>
2021-02-04 12:31:13 +0300

    resolved

--- calculator.js
+++ calculator.js
@@ -1,11 +1,14 @@
-let x =10
-let y =5
-let z=(x/y)
+let x = 10
+let y = 5
+let z=("*")
 
-if (z === 15) {console.log( "addition"); }
-if (z === 5) {console.log( "subtraction"); }
-if (z === 2) {console.log( "division"); }
-if (z === 50) {console.log( "multiplication"); }
+let result; 
+if (z === "+") result = x + y; 
+else if ( z === "-") result = x - y;
+else if ( z === "*") result = x * y;
+else if ( z === "/") result = x / y;
+else { console.log ( " Invalid ")}
+console.log(`${x} ${z} ${y} = ${result}`);
 
 
 
